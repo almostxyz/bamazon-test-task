cd9f97a637f596882889b31a536d3e30d86e6771
Ruslan <almostz@yandex.ru>
2020-10-06 22:26:27 +0400

    :sparkles: add new wrapper variant

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Box } from '@chakra-ui/core'
 
-export const Wrapper: React.FC = ({ children }) => {
+export type WrapperVariant = 'small' | 'regular'
+
+interface WrapperProps {
+    variant?: WrapperVariant 
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({ children, variant = 'regular' }) => {
     return (
         <Box
             mt={8}
             mx='auto'
-            maxW='400px'
-            w='100%'
-        >
+            maxW={variant === 'regular' ? '800px' : '400px'}
+            w='100%'>
             {children}
         </Box>
     )
