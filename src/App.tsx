4bfbf02104c295023bb7cb97c4e9dc8fcc31eb99
Ruslan <almostz@yandex.ru>
2020-10-06 22:26:45 +0400

    :sparkles: add books page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,36 +13,47 @@ import { Login } from './pages/Login';
 import { Profile } from './pages/Profile';
 import { Books } from './pages/Books';
 
+import data from './data/data.json'
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
+import { Provider } from 'react-redux'
+
+const store = createStore(
+  rootReducer
+)
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CSSReset />
+      <Provider store={store}>
+        <Navbar />
+        <Router>
+          <Switch>
+            <Route path='/login' component={Login} />
 
-      <Navbar />
-      <Router>
-        <Switch>
-          <Route path='/login' component={Login} />
+            <PrivateRoute
+              path='/books'
+              Component={Books}
+              props={data}
+              redirectTo='/login'
+            />
 
-          <PrivateRoute
-            path='/books'
-            Component={Books}
-            redirectTo='/login'
-          />
+            <PrivateRoute
+              path='/profile'
+              Component={Profile}
+              redirectTo='/login'
+            />
 
-          <PrivateRoute
-            path='/profile'
-            Component={Profile}
-            redirectTo='/login'
-          />
-
-          <Route exact path='/'>
-            <div>
-              main page
+            <Route exact path='/'>
+              <div>
+                main page
             </div>
-          </Route>
+            </Route>
 
-        </Switch>
-      </Router>
+          </Switch>
+        </Router>
+      </Provider>
     </ThemeProvider>
   )
 }
--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, Button, Flex, Text } from '@chakra-ui/core'
+import { useDispatch, useSelector } from 'react-redux'
+import { Wrapper } from '../components/Wrapper'
+import { Book } from '../constants/types'
+import { getAllBooks, getBooksCount, setAllBooks } from '../reducers/books'
+import { getCurrentPage, getPageBooks, getPageSize, setPage, setPageBooks } from '../reducers/pages'
 
-export const Books: React.FC<{}> = () => {
+interface BooksProps {
+    children?: React.ReactNode,
+    data?: Book[]
+}
+
+export const Books: React.FC<BooksProps> = (props) => {
+
+    const [pagesArray, setPagesArray] = useState([0])
+
+    const bookStore = useSelector((state: any) => state.booksReducer)
+    const pageStore = useSelector((state: any) => state.pagesReducer)
+
+    const dispatch = useDispatch()
+    const changePage = (pageNumber: number) => {
+        dispatch(setPage(pageNumber))
+        const size = getPageSize(pageStore)
+        let newArray = getAllBooks(bookStore).slice((pageNumber - 1) * size, pageNumber * size)
+        dispatch(setPageBooks(newArray))
+    }
+
+    useEffect(() => {
+        dispatch(setAllBooks(props.data!))
+        console.log('1')
+    }, [dispatch, props.data])
+
+    useEffect(() => {
+        let array = Array.from({ length: Math.ceil(getBooksCount(bookStore) / getPageSize(pageStore)) }, (_, i) => i + 1)
+        setPagesArray(array)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [bookStore])
+
+    useEffect(() => {
+        changePage(1)
+        console.log('3')
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [bookStore])
+
+
     return (
-        <div>
-            Books Page
-        </div>
+        <Wrapper variant='regular'>
+            <Box minH={180}>
+                {
+                    getPageBooks(pageStore).map((b) => {
+                        return (
+                            <Flex
+                                key={b.id}
+                                justifyContent={'space-between'}
+                            >
+                                <Text fontSize="2xl">
+                                    {b.title}
+                                </Text>
+                                <Text fontSize="2xl">
+                                    {b.author}
+                                </Text>
+                            </Flex>
+                        )
+                    })
+                }
+            </Box>
+            <Flex
+                justify='center'
+            >
+                {
+                    pagesArray.map((p) => {
+                        return (
+                            <Button
+                                mr={2}
+                                variantColor='teal'
+                                isActive={p === getCurrentPage(pageStore)}
+                                key={p}
+                                onClick={() => {
+                                    changePage(p)
+                                }}
+                            >{p}
+                            </Button>
+                        )
+                    })
+                }
+            </Flex>
+        </Wrapper>
     )
 }
+
