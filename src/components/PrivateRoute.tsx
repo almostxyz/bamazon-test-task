988d60594de78de28ae719402e62da8a9afb970e
Ruslan <almostz@yandex.ru>
2020-10-06 22:25:15 +0400

    :bug: fix a bug with props pass

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Redirect, Route } from 'react-router-dom'
 
 interface PrivateRouteProps {
     path: string,
     Component: React.FC,
-    redirectTo: string
+    redirectTo: string,
+    props?: any
 }
 
-export const PrivateRoute: React.FC<PrivateRouteProps> = ({ path, Component, redirectTo }) => {
+export const PrivateRoute: React.FC<PrivateRouteProps> = ({ path, Component, redirectTo, props }) => {
     return (
         <Route path={path}>
             {
                 localStorage.getItem('logged_in') === 'true' ?
-                    <Component /> :
+                    <Component {...props} /> :
                     <Redirect to={redirectTo} />
             }
         </Route>
